694595fcb477e3030843c66b92fd11359f6acd7c
Varad-Dev-17 <Varad.Mule@cybaemtech.com>
2026-06-02 13:09:33 +0530

    Add todos to list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
 import styles from "./App.module.css";
 import TodoForm from "./components/TodoForm/TodoForm";
+import { useState } from "react";
+import TodoList from "./components/TodoList/TodoList";
 
+const TODOS_DEFAULT = [
+  {
+    id: "1",
+    name: "Buy an Ice Cream",
+    description: "The white one with chocolate",
+    deadline: "2025-02-09",
+    priority: "low",
+    completed: true,
+  },
+  {
+    id: "2",
+    name: "Sell old MacBook Pro 2025",
+    description: "Try to sell it on OLX",
+    deadline: "2025-02-28",
+    priority: "high",
+    completed: false,
+  },
+  {
+    id: "3",
+    name: "Charge Powerbank",
+    description: "For the next travelling",
+    deadline: "2025-02-15",
+    priority: "medium",
+    completed: false,
+  },
+  {
+    id: "4",
+    name: "Test Todo only with a name",
+    description: "",
+    deadline: "",
+    priority: "none",
+    completed: false,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(TODOS_DEFAULT);
+
+  function handleCreation(newTodo) {
+    setTodos((prev) => [...prev, { id: `${prev.length + 1}`, ...newTodo }]);
+  }
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
@@ -11,7 +53,9 @@ function App() {
       </header>
 
       <div className={styles.AppContainer}>
-        <TodoForm />
+        <TodoForm onCreate={handleCreation} />
+        {/* {JSON.stringify(todos)} */}
+        <TodoList todos={todos} />
       </div>
     </div>
   );
--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,21 +1,48 @@
-import React from "react";
-import styles from "../styles/TodoForm.module.css";
-const TodoForm = () => {
+import React, { useState } from "react";
+import styles from "../../styles/TodoForm.module.css";
+// todo-list-react\src\styles\TodoForm.module.css
+
+const TodoForm = ({ onCreate }) => {
+  const [showAllFields, setShowAllFields] = useState(false);
+
   // Submit handler for the form.
   function handleSubmit(e) {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const name = formData.get("name");
-    const description = formData.get("description");
-    const deadline = formData.get("deadline");
-    const priority = formData.get("priority");
 
-    console.log({ name, description, deadline, priority });
+    const { elements } = e.target;
+
+    if (
+      !elements.name.value ||
+      !elements.description.value ||
+      !elements.deadline.value ||
+      !elements.priority.value
+    ) {
+      alert("All fields are required");
+      return;
+    }
+
+    onCreate({
+      name: elements.name.value,
+      description: elements.description.value,
+      deadline: elements.deadline.value,
+      priority: elements.priority.value,
+      completed: false,
+    });
+
+    e.target.reset();
   }
 
   return (
     <section>
-      <h2 className={styles.Title}>New To-Do</h2>
+      <h2 className={styles.Title}>
+        New To-Do
+        <button
+          className={styles.HideButton}
+          onClick={() => setShowAllFields(!showAllFields)}
+        >
+          {showAllFields ? "Hide " : "Show "} Fields
+        </button>
+      </h2>
 
       <form className={styles.Form} onSubmit={handleSubmit}>
         <div className={styles.FormFields}>
@@ -23,37 +50,41 @@ const TodoForm = () => {
             <input
               type="text"
               aria-label="Name*"
-              placeholder="Name*"
+              placeholder="Enter Task Name..."
               name="name"
               autoComplete="off"
             />
           </div>
 
-          <div className={styles.FormField}>
-            <textarea
-              aria-label="Description"
-              placeholder="Description"
-              name="description"
-              rows="3"
-            />
-          </div>
+          {showAllFields && (
+            <>
+              <div className={styles.FormField}>
+                <textarea
+                  aria-label="Description"
+                  placeholder="Enter Task Description..."
+                  name="description"
+                  rows="3"
+                />
+              </div>
 
-          <div className={styles.FormGroup}>
-            <div className={styles.FormField}>
-              <label htmlFor="deadline">Deadline</label>
-              <input type="date" id="deadline" name="deadline" />
-            </div>
+              <div className={styles.FormGroup}>
+                <div className={styles.FormField}>
+                  <label htmlFor="deadline">Deadline</label>
+                  <input type="date" id="deadline" name="deadline" />
+                </div>
 
-            <div className={styles.FormField}>
-              <label htmlFor="priority">Priority</label>
-              <select defaultValue="none" id="priority" name="priority">
-                <option value="none">None</option>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-              </select>
-            </div>
-          </div>
+                <div className={styles.FormField}>
+                  <label htmlFor="priority">Priority</label>
+                  <select defaultValue="none" id="priority" name="priority">
+                    <option value="none">None</option>
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                  </select>
+                </div>
+              </div>
+            </>
+          )}
         </div>
 
         <input type="submit" value="Add" />
